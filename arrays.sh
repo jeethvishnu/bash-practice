d445f9dc78b1c92b60ad5d8f38ff2c44bc05fe46
vishnu <jeeth.pam99@gmail.com>
2024-11-17 16:23:14 +0530

    new

--- arrays.sh
+++ arrays.sh
@@ -4,4 +4,4 @@ fruits=('apple','banana','pineapple')
 echo 'first fruit:$fruits[0]'
 echo "second fruit:$fruits[1]"
 echo "third fruit:$fruits[2]"
-echo "all fruits"$fruit[@]
+echo "all fruits"$fruits[@]"
