d375804f37443e94c50731561cb5ae5bb19f98ca
vishnu <jeeth.pam99@gmail.com>
2024-11-17 15:24:36 +0530

    changes made

--- variables.sh
+++ variables.sh
@@ -3,7 +3,7 @@ PERSON1=jeeth
 PERSON2=sheetal
 
 echo "$PERSON1:: hi there"
-echo "$PERSON2: hi PERSON1, how are you"
+echo "$PERSON2: hi $PERSON1, how are you"
 echo "$PERSON1: i am fine WBU?"
 echo "$PERSON2::i am good"
 echo "$PERSON1:: okay"
