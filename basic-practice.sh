a247d8b2a327cc93b2c79ac3af2aea241bc1509e
vishnu <jeeth.pam99@gmail.com>
2024-11-18 20:07:24 +0530

    change

--- basic-practice.sh
+++ basic-practice.sh
@@ -2,11 +2,18 @@
 
 # echo 'hello from the other side'
 
-p1=sheet
-p2=jeeth
-echo "$p1:hi there"
-echo "$p2:whats up"
-echo "$p1:fine about you?"
-echo "$p2:i'm good"
-echo "$p1:bye from this side"
-echo "$p2:bye $p1"
+# p1=sheet
+# p2=jeeth
+# echo "$p1:hi there"
+# echo "$p2:whats up"
+# echo "$p1:fine about you?"
+# echo "$p2:i'm good"
+# echo "$p1:bye from this side"
+# echo "$p2:bye $p1"
+
+echo "enter username"
+read -s username
+echo "enter passwd"
+read -s passwd
+echo "your username is: $username"
+echo "your passwd is: $passwd"
