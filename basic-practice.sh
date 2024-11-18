8d4835f20cbed631b0fb1646c2998159345ce62f
vishnu <jeeth.pam99@gmail.com>
2024-11-18 20:13:34 +0530

    change

--- basic-practice.sh
+++ basic-practice.sh
@@ -11,9 +11,17 @@
 # echo "$p1:bye from this side"
 # echo "$p2:bye $p1"
 
-echo "enter username"
-read -s username
-echo "enter passwd"
-read -s passwd
-echo "your username is: $username"
-echo "your passwd is: $passwd"
+# echo "enter username"
+# read -s username
+# echo "enter passwd"
+# read -s passwd
+# echo "your username is: $username"
+# echo "your passwd is: $passwd"
+
+a=('apple' 'banana' 'grapes')
+echo "first element is: ${a[0]}"
+echo "second element is: ${a[1]}"
+echo "third element is: ${a[2]}"
+
+echo "all elemnts: ${a[@]}"
+
