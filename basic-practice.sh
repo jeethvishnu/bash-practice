81c811170cd674e98e20be06626dc9dde4e272b9
vishnu <jeeth.pam99@gmail.com>
2024-11-18 20:04:02 +0530

    changes

--- basic-practice.sh
+++ basic-practice.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo 'hello from the other side'
+# echo 'hello from the other side'
+
+p1=sheet
+p2=jeeth
+echo "$p1:hi there"
+echo "$p2:whats up"
+echo "$p1:fine about you?"
+echo "$p2:i'm good"
+echo "$p1:bye from this side"
+echo "$p2:bye $p1"
