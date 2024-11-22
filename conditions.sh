adc6d82f8dd5c7d1a26400c6036b679d0b1186ce
jeethvishnu <jeeth.pam99@gmail.com>
2024-11-22 18:10:50 +0530

    edited

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-num=$1
+# num=$1
 
-if [ $num -gt 10 ]
+# if [ $num -gt 10 ]
+# then
+#     echo "given num $num is greater than 10"
+# else
+#     echo "given num $num is not greater than 10"
+# fi
+
+# practice
+
+num1=$1
+num2=$2
+if [ $num1 -gt $num2 ]
 then
-    echo "given num $num is greater than 10"
+    echo "number $num1 is gretaer than num2"
 else
-    echo "given num $num is not greater than 10"
+    echo "number $num2 is greater tha $num1"
 fi
