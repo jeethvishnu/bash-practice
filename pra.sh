2cf476e5138f96ace63b732c083aa8877450368c
jeethvishnu <jeeth.pam99@gmail.com>
2024-11-22 17:43:00 +0530

    edited

--- pra.sh
+++ pra.sh
@@ -1,8 +1,13 @@
  #/bin/bash
 
- echo "enter username: "
- read -s username
- echo "enter passwd: "
- read -s passwd
- echo "your usrnmane: $username"
- echo "your passwd: $passwd"
+#  echo "enter username: "
+#  read -s username
+#  echo "enter passwd: "
+#  read -s passwd
+#  echo "your usrnmane: $username"
+#  echo "your passwd: $passwd"
+
+n1=$1
+n2=$2
+sum= $(($n1+$n1))
+echo "total is : $sum"
