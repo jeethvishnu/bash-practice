f819e48daffb3209878ffe487587cc0b2a1ea0cd
jeethvishnu <jeeth.pam99@gmail.com>
2024-11-22 18:01:17 +0530

    edited

--- specialvars.sh
+++ specialvars.sh
@@ -1,3 +1,6 @@
 a=('aa' 'bb' 'cc')
 echo "no of variables passed: $#"
 echo "print all vars : ${a[@]}"
+echo "print current working dir : $PWD"
+echo "print script name: $o"
+echo "print home dir: $HOME
