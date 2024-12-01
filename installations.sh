cce204eb6f8b74bad4447199df424cfbed299181
jeethvishnu <jeeth.pam99@gmail.com>
2024-12-01 14:00:56 +0530

    new

--- installations.sh
+++ installations.sh
@@ -6,6 +6,7 @@ user=$(id -u) #check if super user or not first
 if [ $user -ne 0 ]
 then
     echo "please run in super user"
+    exit 1 #manually exit if error comes
 else
     echo "you are super usr"
 fi
