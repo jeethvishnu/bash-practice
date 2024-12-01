8dcb24b2a80fbec4e25beb00977b49f59385605a
jeethvishnu <jeeth.pam99@gmail.com>
2024-12-01 14:37:15 +0530

    new

--- funtions.sh
+++ funtions.sh
@@ -2,11 +2,39 @@
 #!/bin/bash
 #here we are trying to install some pakages
 
+# user=$(id -u) #check if super user or not first
+
+# validate(){
+#     echo "exit status:$1"
+#     echo "are you doing:$2"
+
+# }
+# if [ $user -ne 0 ]
+# then
+#     echo "please run in super user"
+#     exit 1 #manually exit if error comes
+# else
+#     echo "you are super usr"
+# fi
+
+# dnf install mysql -y
+# validate $? "installing sql"
+
+
+# dnf install git -y
+# validate $? "installing git"
+
+#second
+
 user=$(id -u) #check if super user or not first
 
 validate(){
-    echo "exit status:$1"
-    echo "are you doing:$2"
+   if [ $1 -ne 0 ]
+   then
+        echo "$2 fail"
+        exit 1
+    else
+        echo "$2 success"
 
 }
 if [ $user -ne 0 ]
