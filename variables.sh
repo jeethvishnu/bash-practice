9e92f2cba868c822e7d6acfe4866b13ec5e39b39
vishnu <jeeth.pam99@gmail.com>
2024-12-22 13:46:33 +0530

    modified

--- variables.sh
+++ variables.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-p1 =vishnu
-p2 =jeeth
+PERSON1=jeeth
+PERSON2=sheetal
 
-echo "hi $p1 how are you"
-echo "hi $p2 i am good"
-echo "what are you doing $p1"
-echo "nothing much $p2.... just now had lunch"
-echo "okay $p1 bye"
-echo "okay bye $p2"
+echo "$PERSON1:: hi there"
+echo "$PERSON2: hi $PERSON1, how are you"
+echo "$PERSON1: i am fine WBU?"
+echo "$PERSON2::i am good"
+echo "$PERSON1:: okay"
