00e584684edabf649b6e1609bb758713c6a5ba20
vishnu <jeeth.pam99@gmail.com>
2024-12-22 15:34:54 +0530

    modified

--- dumy.sh
+++ dumy.sh
@@ -6,26 +6,26 @@ q=$2
 echo "hi there $p"
 echo "hi $q"
 echo "sign up yoour creds $p and $q"
-echo "both signin creds are:"
-read -s username
-read -s passwd
-echo "your creds are: $username"
-echo "your pass is: $passwd"
+# echo "both signin creds are:"
+# read -s username
+# read -s passwd
+# echo "your creds are: $username"
+# echo "your pass is: $passwd"
 
-echo "lets install some packages $p,$q"
+# echo "lets install some packages $p,$q"
 
-usr=$(id -u)
-if [ $usr -ne 0 ]
-then
-    echo "is it sudo"
-else    
-    echo "IN SUDO"
-fi
+# usr=$(id -u)
+# if [ $usr -ne 0 ]
+# then
+#     echo "is it sudo"
+# else    
+#     echo "IN SUDO"
+# fi
 
-dnf install mysql -y
-if [ $? -ne 0 ]
-then
-    echo "failed.."
-else
-    echo "success.."
-fi
+# dnf install mysql -y
+# if [ $? -ne 0 ]
+# then
+#     echo "failed.."
+# else
+#     echo "success.."
+# fi
