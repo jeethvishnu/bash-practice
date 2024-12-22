59965d4378b0258c77b8b173f5cc7cc608577264
vishnu <jeeth.pam99@gmail.com>
2024-12-22 15:34:02 +0530

    modified

--- dumy.sh
+++ dumy.sh
@@ -1,30 +1,31 @@
-#welcome
 #!/bin/bash
 
-c=$(id -u)
-if [ $c -ne 0 ]
+p=$1
+q=$2
+
+echo "hi there $p"
+echo "hi $q"
+echo "sign up yoour creds $p and $q"
+echo "both signin creds are:"
+read -s username
+read -s passwd
+echo "your creds are: $username"
+echo "your pass is: $passwd"
+
+echo "lets install some packages $p,$q"
+
+usr=$(id -u)
+if [ $usr -ne 0 ]
 then
     echo "is it sudo"
-    exit 1
-else
-    echo "entered into sudo"
+else    
+    echo "IN SUDO"
 fi
 
-
-dnf install curl -y
-
+dnf install mysql -y
 if [ $? -ne 0 ]
 then
-    echo "failed"
-    exit 1
-else        
-    echo "success"
-fi
-
-dnf install wget -y
-if [ $? -ne 0 ]
-then
-    echo "failed"
+    echo "failed.."
 else
-    echo "success"
+    echo "success.."
 fi
