bf306a3affbe8a68927f20bb8a9477c33be30dd2
vishnu <jeeth.pam99@gmail.com>
2024-12-22 13:57:51 +0530

    modified

--- variables.sh
+++ variables.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-p1=jeeth
-p2=sheetal
 
-# echo "$PERSON1:: hi there"
-# echo "$PERSON2: hi $PERSON1, how are you"
-# echo "$PERSON1: i am fine WBU?"
-# echo "$PERSON2::i am good"
-# echo "$PERSON1:: okay"
+p1=vishnu
+p2=shee
 
 echo "hi $p1"
-echo "hi $p2 how are you"
-echo "enter your username and passwd"
+echo "hi $p2"
+echo "how are you $p2"
+echo "i am fine $p1"
+echo "enter your details $p1 and $p2"
+echo "entering $p1 details"
+echo "enter username"
 read username
+echo "enter passwd"
 read passwd
 echo "username added: $username"
+echo "passwd added: $passwd"
+
+echo "enter $p2 details"
+echo "enter name"
+read name
+echo "passwd"
+read passwd
+echo "name added: $name"
 echo "passwd added: $passwd"
