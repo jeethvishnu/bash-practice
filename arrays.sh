861ff43fb6663cb9aab51959f20463b8c4770600
vishnu <jeeth.pam99@gmail.com>
2024-12-22 14:42:29 +0530

    modified

--- arrays.sh
+++ arrays.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-fruits=('apple' 'banana' 'pineapple')
+# fruits=('apple' 'banana' 'pineapple')
 
-echo "first fruit: ${fruits[0]}"
-echo "second fruit: ${fruits[1]}"
-echo "third fruit: ${fruits[2]}"
+# echo "first fruit: ${fruits[0]}"
+# echo "second fruit: ${fruits[1]}"
+# echo "third fruit: ${fruits[2]}"
 
-echo "all fruits: ${fruits[@]}"
+# echo "all fruits: ${fruits[@]}"
+
+lst=('a' 'b' 'c')
+echo "first is ${lst[0]}"
+echo "second is ${lst[1]}"
+echo "third is ${lst[2]}"
+echo "all the elements ${lst[@]}
