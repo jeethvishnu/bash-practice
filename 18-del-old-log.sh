e234b458fd4c47ac1dd95520708f2c2f39eebf48
vishnu jeeth <jeeth.pam99@gmail.com>
2025-01-13 18:40:22 +0530

    file

---
+++ 18-del-old-log.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+src=/tmp/logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $src ]
+then
+    echo "$G src dir exist $N"
+else
+    echo "$Y src $src dont exist $N"
+    exist 1
+fi
