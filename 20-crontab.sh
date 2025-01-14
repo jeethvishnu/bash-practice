557518f61dad2c251d3be79683d6fb10fa9c58fb
vishnu jeeth <jeeth.pam99@gmail.com>
2025-01-14 18:58:23 +0530

    modified

---
+++ 20-crontab.sh
@@ -0,0 +1,7 @@
+#crontab is used for deleting logs file and used fro scheduling purpose.
+
+crontab -l = it will list out
+
+crontab -e = it will let you edit in vi.
+
+visit the site crontab for knowing about syntax for time.
